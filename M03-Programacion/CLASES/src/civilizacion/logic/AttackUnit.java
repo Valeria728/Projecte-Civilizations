6c131211f50480f9152734450ce4f00105a5ff5e
Valeria728 <cinthiavaleriaperdomo@gmail.com>
2026-05-06 05:16:39 +1300

    Update AttackUnit.java

--- M03-Programacion/CLASES/src/civilizacion/logic/AttackUnit.java
+++ M03-Programacion/CLASES/src/civilizacion/logic/AttackUnit.java
@@ -1,1 +1,35 @@
+package civilizacion.logic;
 
+public abstract class AttackUnit implements MilitaryUnit, Variables {
+    protected int armor;
+    protected int initialArmor;
+    protected int baseDamage;
+    protected int experience;
+    protected boolean sanctified;
+
+    // Implementación de métodos comunes para no repetir código
+    @Override
+    public int getActualArmor() {
+        return this.armor;
+    }
+
+    @Override
+    public void takeDamage(int receivedDamage) {
+        this.armor -= receivedDamage;
+    }
+
+    @Override
+    public int getExperience() {
+        return this.experience;
+    }
+
+    @Override
+    public void setExperience(int n) {
+        this.experience = n;
+    }
+
+    @Override
+    public void resetArmor() {
+        this.armor = this.initialArmor;
+    }
+}
