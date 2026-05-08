eba52cdc7f72da8928e19c7056c6fc2895bbb1f1
diegoc432 <56197682+diegoc432@users.noreply.github.com>
2026-05-08 15:49:50 +0200

    Añadida clase ResourceException (Bloque 5)
    Creación de la excepción personalizada para gestionar la falta de recursos. Se lanza cuando el jugador no tiene los materiales necesarios para construir o reclutar. Hereda de la clase Exception de Java.

---
+++ M03-Programacion/ProyectoCivilizacion3/src/civilization/ResourceException.java
@@ -0,0 +1,9 @@
+package civilization;
+
+public class ResourceException extends Exception {
+
+	// Constructor que recibe el mensaje de error
+    public ResourceException(String mensaje) {
+        super(mensaje);
+    }
+}
