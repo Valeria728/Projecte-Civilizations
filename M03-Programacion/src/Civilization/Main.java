6b166145ccb4c85793b831f55a7632208bbb49bd
Mmortium <molcliff@gmail.com>
2026-05-08 16:51:32 +0200

    modificacion para ver valor recursos

--- M03-Programacion/src/Civilization/Main.java
+++ M03-Programacion/src/Civilization/Main.java
@@ -11,7 +11,7 @@ public class Main implements Variables {
     private static ArrayList<String> battleHistory = new ArrayList<>();
     private static int enemyFleetCount = 1;
     
-    // Recursos iniciales del enemigo (según el PDF para que pueda comprar tropas)
+    // Recursos iniciales del enemigo.
     private static int currentEnemyWood = 15000;
     private static int currentEnemyIron = 8000;
     private static int currentEnemyFood = 12000;
@@ -95,16 +95,20 @@ public class Main implements Variables {
                 opc = sc.nextInt();
 
                 if (opc == 1) { // ATAQUE
-                    System.out.println("1.Swordsman 2.Spearman 3.Crossbow 4.Cannon");
+                    System.out.println("1.Swordsman | 2.Spearman | 3.Crossbow | 4.Cannon");
                     int sel = sc.nextInt();
-                    System.out.print("Cantidad: ");
+                    
+                    // Mostramos el precio según la selección
+                    if (sel == 1) System.out.println("[COSTE] Madera:" + WOOD_COST_SWORDSMAN + " Hierro:" + IRON_COST_SWORDSMAN + " Comida:" + FOOD_COST_SWORDSMAN);
+                    else if (sel == 2) System.out.println("[COSTE] Madera:" + WOOD_COST_SPEARMAN + " Hierro:" + IRON_COST_SPEARMAN + " Comida:" + FOOD_COST_SPEARMAN);
+                    else if (sel == 3) System.out.println("[COSTE] Madera:" + WOOD_COST_CROSSBOW + " Hierro:" + IRON_COST_CROSSBOW);
+                    else if (sel == 4) System.out.println("[COSTE] Madera:" + WOOD_COST_CANNON + " Hierro:" + IRON_COST_CANNON);
+                    
+                    System.out.print("¿Cuántos quieres reclutar?: ");
                     int cant = sc.nextInt();
-                    if (sel == 1) myCiv.newSwordsman(cant);
-                    else if (sel == 2) myCiv.newSpearman(cant);
-                    else if (sel == 3) myCiv.newCrossbow(cant);
-                    else if (sel == 4) myCiv.newCannon(cant);
-
-                } else if (opc == 2) { // DEFENSA
+                    
+                }
+                else if (opc == 2) { // DEFENSA
                     System.out.println("1.Arrow Tower 2.Catapult 3.Rocket Launcher");
                     int sel = sc.nextInt();
                     System.out.print("Cantidad: ");
@@ -113,7 +117,8 @@ public class Main implements Variables {
                     else if (sel == 2) myCiv.newCatapult(cant);
                     else if (sel == 3) myCiv.newRocketLauncher(cant);
 
-                } else if (opc == 3) { // ESPECIALES
+                } 
+                else if (opc == 3) { // ESPECIALES
                     System.out.println("1.Magician (Torre) 2.Priest (Iglesia)");
                     int sel = sc.nextInt();
                     System.out.print("Cantidad: ");
@@ -121,7 +126,8 @@ public class Main implements Variables {
                     if (sel == 1) myCiv.newMagician(cant);
                     else if (sel == 2) myCiv.newPriest(cant);
 
-                } else if (opc == 4) { // EDIFICIOS
+                } 
+                else if (opc == 4) { // EDIFICIOS
                     System.out.println("1.Granja 2.Herrería 3.Carpintería 4.Torre 5.Iglesia");
                     int sel = sc.nextInt();
                     if (sel == 1) myCiv.buildFarm();
@@ -130,16 +136,19 @@ public class Main implements Variables {
                     else if (sel == 4) myCiv.buildMagicTower();
                     else if (sel == 5) myCiv.buildChurch();
 
-                } else if (opc == 5) { // TECNOLOGÍA
+                } 
+                else if (opc == 5) { // TECNOLOGÍA
                     System.out.println("1.Ataque 2.Defensa");
                     int sel = sc.nextInt();
                     if (sel == 1) myCiv.upgradeTechnologyAttack();
                     else if (sel == 2) myCiv.upgradeTechnologyDefense();
 
-                } else if (opc == 6) { // REPORTE EJERCITO
+                } 
+                else if (opc == 6) { // REPORTE EJERCITO
                     printArmyStats();
 
-                } else if (opc == 7) { // HISTORIAL
+                } 
+                else if (opc == 7) { // HISTORIAL
                     int i = 0;
                     if (battleHistory.isEmpty()) System.out.println("Sin registros.");
                     while (i < battleHistory.size()) {
@@ -147,14 +156,17 @@ public class Main implements Variables {
                         i++;
                     }
 
-                } else if (opc == 8) { // AMENAZA
+                } 
+                else if (opc == 8) { // AMENAZA
                     viewThreat();
 
-                } else if (opc == 9) { // GUARDAR
+                } 
+                else if (opc == 9) { // GUARDAR
                     dao.saveGame(myCiv);
                     System.out.println("Partida guardada.");
 
-                } else if (opc == 10) { // SALIR
+                } 
+                else if (opc == 10) { // SALIR
                     dao.saveGame(myCiv);
                     System.out.println("Cerrando...");
                 }
@@ -167,7 +179,7 @@ public class Main implements Variables {
                 System.err.println("Error en entrada.");
                 sc.next();
             }
-        } // Fin while
+        } 
 
         timer.cancel();
         sc.close();
@@ -213,19 +225,23 @@ public class Main implements Variables {
         // El enemigo gasta sus recursos en unidades de ataque (de mejor a peor)
         while (iron >= IRON_COST_CANNON && wood >= WOOD_COST_CANNON) {
             enemyArmy[3].add(new Cannon());
-            iron -= IRON_COST_CANNON; wood -= WOOD_COST_CANNON;
+            iron -= IRON_COST_CANNON; 
+            wood -= WOOD_COST_CANNON;
         }
         while (iron >= IRON_COST_CROSSBOW && wood >= WOOD_COST_CROSSBOW) {
             enemyArmy[2].add(new Crossbow());
-            iron -= IRON_COST_CROSSBOW; wood -= WOOD_COST_CROSSBOW;
+            iron -= IRON_COST_CROSSBOW; 
+            wood -= WOOD_COST_CROSSBOW;
         }
         while (food >= FOOD_COST_SPEARMAN && wood >= WOOD_COST_SPEARMAN) {
             enemyArmy[1].add(new Spearman());
-            food -= FOOD_COST_SPEARMAN; wood -= WOOD_COST_SPEARMAN;
+            food -= FOOD_COST_SPEARMAN; 
+            wood -= WOOD_COST_SPEARMAN;
         }
         while (food >= FOOD_COST_SWORDSMAN && wood >= WOOD_COST_SWORDSMAN) {
             enemyArmy[0].add(new Swordsman());
-            food -= FOOD_COST_SWORDSMAN; wood -= WOOD_COST_SWORDSMAN;
+            food -= FOOD_COST_SWORDSMAN; 
+            wood -= WOOD_COST_SWORDSMAN;
         }
         return enemyArmy;
     }
