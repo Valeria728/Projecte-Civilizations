606d29046f06fff7b314275cf367ace3023b82e0
diegoc432 <56197682+diegoc432@users.noreply.github.com>
2026-05-12 19:36:55 +0200

    Añadida clase ArrowTower
    Implementada la torre de flechas con cálculo de estadísticas basado en tecnología y costes de recursos.

--- M03-Programacion/units/defense/ArrowTower.java
+++ M03-Programacion/units/defense/ArrowTower.java
@@ -1,14 +1,35 @@
 package units.defense;
+
 import units.DefenseUnit;
 
-public class ArrowTower extends DefenseUnit{
-    //necessitaeis une segundo constructor que crea el ArrowTower calculando la technlogia de attack y defense
-	public ArrowTower(int armor, int baseDamage) { super(armor, baseDamage); }
-    @Override public int attack() { return baseDamage; }
-    @Override public int getFoodCost() { return FOOD_COST_ARROWTOWER; }
-    @Override public int getWoodCost() { return WOOD_COST_ARROWTOWER; }
-    @Override public int getIronCost() { return IRON_COST_ARROWTOWER; }
-    @Override public int getManaCost() { return MANA_COST_ARROWTOWER; }
-    @Override public int getChanceGeneratinWaste() { return CHANCE_GENERATNG_WASTE_ARROWTOWER; }
-    @Override public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_ARROWTOWER; }
+public class ArrowTower extends DefenseUnit {
+
+    // El constructor recibe el nivel de tecnología para calcular stats iniciales
+    public ArrowTower(int technologyDefense, int technologyAttack) {
+        super(
+            ARMOR_ARROWTOWER + (technologyDefense * PLUS_ARMOR_ARROWTOWER_BY_TECHNOLOGY * ARMOR_ARROWTOWER / 100),
+            BASE_DAMAGE_ARROWTOWER + (technologyAttack * PLUS_ATTACK_ARROWTOWER_BY_TECHNOLOGY * BASE_DAMAGE_ARROWTOWER / 100)
+        );
+    }
+
+    @Override
+    public int getFoodCost() { return FOOD_COST_ARROWTOWER; }
+
+    @Override
+    public int getWoodCost() { return WOOD_COST_ARROWTOWER; }
+
+    @Override
+    public int getIronCost() { return IRON_COST_ARROWTOWER; }
+
+    @Override
+    public int getManaCost() { return MANA_COST_ARROWTOWER; }
+
+    @Override
+    public int getChanceGeneratingWaste() { return CHANCE_GENERATNG_WASTE_ARROWTOWER; }
+
+    @Override
+    public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_ARROWTOWER; }
+
+    @Override
+    public String toString() { return "Torre de Flechas"; }
 }
