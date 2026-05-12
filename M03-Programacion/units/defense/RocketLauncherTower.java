f96d303c1e6222a7257611ff7cdffbbf65992c08
diegoc432 <56197682+diegoc432@users.noreply.github.com>
2026-05-12 19:43:37 +0200

    Añadida clase RocketLauncherTower
    Finalizado el bloque de unidades de defensa. Implementada la torre lanzacohetes con escalado tecnológico.

--- M03-Programacion/units/defense/RocketLauncherTower.java
+++ M03-Programacion/units/defense/RocketLauncherTower.java
@@ -2,15 +2,33 @@ package units.defense;
 
 import units.DefenseUnit;
 
-public class RocketLauncherTower extends DefenseUnit{
-    //necessitaeis une segundo constructor que crea el RocketLauncherTower calculando la technlogia de attack y defense
-	public RocketLauncherTower(int armor, int baseDamage) { super(armor, baseDamage); }
-    @Override public int attack() { return baseDamage; }
-    @Override public int getFoodCost() { return FOOD_COST_ROCKETLAUNCHERTOWER; }
-    @Override public int getWoodCost() { return WOOD_COST_ROCKETLAUNCHERTOWER; }
-    @Override public int getIronCost() { return IRON_COST_ROCKETLAUNCHERTOWER; }
-    @Override public int getManaCost() { return MANA_COST_ROCKETLAUNCHERTOWER; }
-    @Override public int getChanceGeneratinWaste() { return CHANCE_GENERATNG_WASTE_ROCKETLAUNCHERTOWER; }
-    @Override public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_ROCKETLAUNCHERTOWER; }
+public class RocketLauncherTower extends DefenseUnit {
 
+    public RocketLauncherTower(int technologyDefense, int technologyAttack) {
+        super(
+            ARMOR_ROCKETLAUNCHERTOWER + (technologyDefense * PLUS_ARMOR_ROCKETLAUNCHERTOWER_BY_TECHNOLOGY * ARMOR_ROCKETLAUNCHERTOWER / 100),
+            BASE_DAMAGE_ROCKETLAUNCHERTOWER + (technologyAttack * PLUS_ATTACK_ROCKETLAUNCHERTOWER_BY_TECHNOLOGY * BASE_DAMAGE_ROCKETLAUNCHERTOWER / 100)
+        );
+    }
+
+    @Override
+    public int getFoodCost() { return FOOD_COST_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public int getWoodCost() { return WOOD_COST_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public int getIronCost() { return IRON_COST_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public int getManaCost() { return MANA_COST_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public int getChanceGeneratingWaste() { return CHANCE_GENERATNG_WASTE_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_ROCKETLAUNCHERTOWER; }
+
+    @Override
+    public String toString() { return "Torre Lanzacohetes"; }
 }
