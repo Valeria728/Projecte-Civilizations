d5f1196ac2592ab38f419a83af2a03f9db0fc9cf
diegoc432 <56197682+diegoc432@users.noreply.github.com>
2026-05-12 19:40:56 +0200

    Añadida clase Catapult
    Implementación de la unidad de defensa Catapult con escalado por tecnología.

--- M03-Programacion/units/defense/Catapult.java
+++ M03-Programacion/units/defense/Catapult.java
@@ -2,14 +2,33 @@ package units.defense;
 
 import units.DefenseUnit;
 
-public class Catapult extends DefenseUnit{
-    //necessitaeis une segundo constructor que crea el Catapult calculando la technlogia de attack y defense
-	public Catapult(int armor, int baseDamage) { super(armor, baseDamage); }
-    @Override public int attack() { return baseDamage; }
-    @Override public int getFoodCost() { return FOOD_COST_CATAPULT; }
-    @Override public int getWoodCost() { return WOOD_COST_CATAPULT; }
-    @Override public int getIronCost() { return IRON_COST_CATAPULT; }
-    @Override public int getManaCost() { return MANA_COST_CATAPULT; }
-    @Override public int getChanceGeneratinWaste() { return CHANCE_GENERATNG_WASTE_CATAPULT; }
-    @Override public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_CATAPULT; }
+public class Catapult extends DefenseUnit {
+
+    public Catapult(int technologyDefense, int technologyAttack) {
+        super(
+            ARMOR_CATAPULT + (technologyDefense * PLUS_ARMOR_CATAPULT_BY_TECHNOLOGY * ARMOR_CATAPULT / 100),
+            BASE_DAMAGE_CATAPULT + (technologyAttack * PLUS_ATTACK_CATAPULT_BY_TECHNOLOGY * BASE_DAMAGE_CATAPULT / 100)
+        );
+    }
+
+    @Override
+    public int getFoodCost() { return FOOD_COST_CATAPULT; }
+
+    @Override
+    public int getWoodCost() { return WOOD_COST_CATAPULT; }
+
+    @Override
+    public int getIronCost() { return IRON_COST_CATAPULT; }
+
+    @Override
+    public int getManaCost() { return MANA_COST_CATAPULT; }
+
+    @Override
+    public int getChanceGeneratingWaste() { return CHANCE_GENERATNG_WASTE_CATAPULT; }
+
+    @Override
+    public int getChanceAttackAgain() { return CHANCE_ATTACK_AGAIN_CATAPULT; }
+
+    @Override
+    public String toString() { return "Catapulta"; }
 }
